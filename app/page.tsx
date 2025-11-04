b713b6f06c081626fdce662038ad2795d1d86e68
puneethkanike09 <reachout.puneeth@gmail.com>
2025-11-04 12:53:58 +0530

    Update layout to suppress hydration warning and adjust heading color in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-red-500 font-bold underline">Hello World</h1>
+      <h1 className="text-3xl text-red-600 font-bold underline">Hello World</h1>
     </div>
   );
 }
