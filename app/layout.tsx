5bbd8fdb21042910dfb85253a8a6c9d5ee1c2c17
puneethkanike09 <reachout.puneeth@gmail.com>
2025-11-04 11:27:48 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,9 +24,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head />
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
       </body>
     </html>
