52cf389b4b9849bcb55bf35398351168536ed05a
puneethkanike09 <reachout.puneeth@gmail.com>
2025-11-08 16:33:18 +0530

    Remove commented-out ThemeToggle component from Navbar for cleaner code.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -102,8 +102,6 @@ export default function Navbar() {
                                 />
                             </button>
 
-                            {/* Theme Toggle */}
-                            {/* <ThemeToggle /> */}
                         </div>
                     </div>
                 </div>
